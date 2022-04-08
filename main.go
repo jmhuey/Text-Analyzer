66a094c47f09c3b5ea5481bbf12690124eb3f01e
jmhuey <jonathan@hueyfamily.com>
2022-04-07 18:35:12 -0700

    added code to check for numbers found

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -28,8 +29,6 @@ func main() {
 		// Temporarily added to focus on letters only -----------------------
 		if !isLetter(token) && !isNumber(token) {
 			continue
-		} else if isNumber(token) {
-			continue
 		}
 		// ------------------------------------------------------------------
 
@@ -37,6 +36,7 @@ func main() {
 			token = strings.ToLower(token)
 		}
 
+		// ignores the whitespace | if the token is in the map add 1 to the value otherwise add the token to map
 		switch []rune(token)[0] {
 		case ' ', '\t', '\n', '\f', '\r', '\v':
 			continue
@@ -52,28 +52,67 @@ func main() {
 
 	}
 
-	letters_found := make([]string, 0, len(char))
+	letters_found := make([]string, 0)
 	letters_notFound := make([]string, 0)
 
+	num_found := make([]string, 0)
+	num_notFound := make([]int, 0)
+
+	// sorting the characters found
 	for k := range char {
-		letters_found = append(letters_found, k)
+		if isLetter(k) {
+			letters_found = append(letters_found, k)
+		} else if isNumber(k) {
+			num_found = append(num_found, k)
+		}
 	}
 
+	// sort letters and numbers alphabetically and numerically respectively
 	sort.Strings(letters_found)
+	sort.Strings(num_found)
 
-	// checks if there were any letters not used in the text
+	// checks if there were any letters not found in the text
 	for l := 'a'; l < 'z'; l++ {
 		if _, ok := char[string(l)]; !ok {
 			letters_notFound = append(letters_notFound, string(l))
 		}
 	}
 
-	fmt.Println("These are the letters found in the text:")
-	for _, letter := range letters_found {
-		fmt.Println(letter, *char[letter])
+	// checks if there were any numbers not found in text
+	for n := 0; n < 10; n++ {
+		if _, ok := char[strconv.Itoa(n)]; !ok {
+			num_notFound = append(num_notFound, n)
+		}
 	}
 
+	// prints the numbers found
+	fmt.Println("These are the numbers found in the text:")
+
+	for _, num := range num_found {
+		fmt.Println(num, ": ", *char[num])
+	}
+
+	// prints out numbers not found if any
+	if len(num_notFound) != 0 {
+		fmt.Println()
+		fmt.Println("These are the numbers not found in the text:")
+
+		for _, num := range num_notFound {
+			fmt.Println(num)
+		}
+	}
+
+	// prints out letters found
+	fmt.Println()
+	fmt.Println("These are the letters found in the text:")
+
+	for _, letter := range letters_found {
+		fmt.Println(letter, ": ", *char[letter])
+	}
+
+	// prints out letters not found if any
 	if len(letters_notFound) != 0 {
+		fmt.Println()
 		fmt.Println("These are the letters not found in the text:")
 
 		for _, letter := range letters_notFound {
