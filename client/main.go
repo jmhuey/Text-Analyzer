87357110f4cc096a055c2abf147a607821c06f4b
jmhuey <jonathan@hueyfamily.com>
2022-04-09 14:38:19 -0700

    created client to send requests

---
+++ client/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	client := &http.Client{}
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	fw, err := writer.CreateFormFile("file", "test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = io.Copy(fw, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writer.Close()
+
+	req, err := http.NewRequest("POST", "http://127.0.0.1:8080/", body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(content)
+}
